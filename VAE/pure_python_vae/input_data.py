78d7980490bcf651cd05799d82edae5afa413ff0
zmr <zmr@uchicago.edu>
2018-06-20 16:59:54 -0500

    added base image for display

--- VAE/pure_python_vae/input_data.py
+++ VAE/pure_python_vae/input_data.py
@@ -20,8 +20,7 @@ def maybe_download(filename, work_directory):
 
 def _read32(bytestream):
     dt = numpy.dtype(numpy.uint32).newbyteorder('>')
-    return numpy.frombuffer(bytestream.read(4), dtype=dt)
-
+    return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
 
 def extract_images(filename):
     """Extract the images into a 4D uint8 numpy array [index, y, x, depth]."""
